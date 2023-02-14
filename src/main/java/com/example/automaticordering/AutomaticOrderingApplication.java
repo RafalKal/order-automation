d330e2bee19b983b33fc5e0f24d61b4e83abd018
Rafal_Kalandyk <rkalandyk@gmail.com>
2023-02-14 17:22:40 +0100

    FIRST COMMIT AND PUSH

---
+++ src/main/java/com/example/automaticordering/AutomaticOrderingApplication.java
@@ -0,0 +1,14 @@
+package com.example.automaticordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticOrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticOrderingApplication.class, args);
+        System.out.println("HELLO WORLD");
+    }
+
+}
---
+++ src/test/java/com/example/automaticordering/AutomaticOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automaticordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
